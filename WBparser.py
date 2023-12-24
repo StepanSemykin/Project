76be010d8c0500e0db3a6b7f537002821be3b842
Степан Семыкин <semykinstepan2@gmail.com>
2023-12-25 00:22:06 +0400

    Переработана логика парсера

--- WBparser.py
+++ WBparser.py
@@ -1,67 +1,26 @@
 import multiprocessing as mp
 import time
+import random
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
-URL = 'https://www.wildberries.ru/catalog/0/search.aspx?page=1&sort=%s&search=%s'
-PRODUCTS_PER_PAGE = 99
-GOODS_PER_PAGE = 29
+URL = 'https://www.wildberries.ru/catalog/0/search.aspx?page=%d&sort=%s&search=%s'
+PRODUCTS_PER_LOADED_PAGE = 100
+PRODUCTS_PER_PAGE = 30
 
 
-def get_valid_url(search_name: str, sort: str) -> str:
-    search_name = search_name.replace(' ', '+')
-    return URL % (sort, search_name)
-
-
-def get_valid_index(index: int) -> int:
-    return int(str(index)[-2:])
-
-
-def extract_data(search_name: str, quantity: int, sort: str) -> list:
-    url = get_valid_url(search_name, sort)
-    args = [(url, i) for i in range(quantity)]
-    results_list = []
-    with mp.Pool(processes=mp.cpu_count()) as p:
-        result = p.starmap(get_info, args)
-        results_list.append(result)
-    return results_list[0]
-
-
-def get_info(url: str, index: int) -> list:
+def get_info(valid_url: str) -> list:
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--start-maximized')
-    chrome_options.add_argument('--remote-debugging-port=0')
-    service = webdriver.ChromeService(port=0)
-    driver = webdriver.Chrome(options=chrome_options, service=service)
+    chrome_options.add_argument('--headless')
+
+    driver = webdriver.Chrome(options=chrome_options)
     driver.set_page_load_timeout(20)
 
-    page = str((index // PRODUCTS_PER_PAGE) + 1)
-    valid_url = url.replace('1', page)
     try:
         driver.get(url=valid_url)
-
-        if index > GOODS_PER_PAGE:
-            index = get_valid_index(index)
-            time.sleep(2)
-            new_height = driver.execute_script("return document.body.scrollHeight") 
-            driver.execute_script(f"window.scrollTo(0, {new_height * 0.5});")
-            time.sleep(3)
-            driver.execute_script(f"window.scrollTo({new_height * 0.5}, {new_height * 0.9});")
-            time.sleep(3)
-            driver.execute_script(f"window.scrollTo({new_height * 0.9}, {new_height * 1.2});")
-            time.sleep(3)
-            driver.execute_script(f"window.scrollTo({new_height * 1.2}, {new_height * 1.4});")
-            time.sleep(3)
-            driver.execute_script(f"window.scrollTo({new_height * 1.4}, {new_height * 1.8});")
-            time.sleep(3)   
-
-        goods = WebDriverWait(driver, 20).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, 'product-card')))
-        goods[index].click()
 
         product_name = WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.CLASS_NAME, 'product-page__header')))
@@ -79,15 +38,96 @@ def get_info(url: str, index: int) -> list:
 
         res = [product_name.text, id, rating.text,
                number_ratings.text, old_price.text, current_price.text]
-        
+
     except Exception as err:
         print(err)
         driver.quit()
-        return [None, index]  
+        return [None, valid_url]
     driver.quit()
     return res
 
 
+def get_valid_url(search_name: str, sort: str, page: int) -> str:
+    search_name = search_name.replace(' ', '+')
+    return URL % (page, sort, search_name)
+
+
+def get_links(quantity: int, valid_url: str):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--start-maximized')
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.set_page_load_timeout(20)
+
+    try:
+        driver.get(url=valid_url)
+        time.sleep(3)
+
+        time_ = 5
+        if quantity > 30:
+            height = driver.execute_script("return document.body.scrollHeight")
+            if 30 < quantity <= 45:
+                driver.execute_script(f"window.scrollTo(0, {height * 0.5});")  # 45
+                time.sleep(time_)
+            elif 45 < quantity <= 60:
+                driver.execute_script(f"window.scrollTo(0, {height * 0.5});")  # 45
+                time.sleep(time_)
+                driver.execute_script(f"window.scrollTo(0, {height * 0.9});")  # 60
+                time.sleep(time_)
+            elif 60 < quantity <= 75:
+                driver.execute_script(f"window.scrollTo(0, {height * 0.5});")  # 45
+                time.sleep(time_)
+                driver.execute_script(f"window.scrollTo(0, {height * 0.9});")  # 60
+                time.sleep(time_)
+                driver.execute_script(f"window.scrollTo(0, {height * 1.3});")  # 75
+                time.sleep(time_)
+            elif 75 < quantity <= 90:
+                driver.execute_script(f"window.scrollTo(0, {height * 0.5});")  # 45
+                time.sleep(time_)
+                driver.execute_script(f"window.scrollTo(0, {height * 0.9});")  # 60
+                time.sleep(time_)
+                driver.execute_script(f"window.scrollTo(0, {height * 1.3});")  # 75
+                time.sleep(time_)
+                driver.execute_script(f"window.scrollTo(0, {height * 1.7});")  # 90
+                time.sleep(time_)
+            else:
+                driver.execute_script(f"window.scrollTo(0, {height * 0.5});")  # 45
+                time.sleep(time_)
+                driver.execute_script(f"window.scrollTo(0, {height * 0.9});")  # 60
+                time.sleep(time_)
+                driver.execute_script(f"window.scrollTo(0, {height * 1.3});")  # 75
+                time.sleep(time_)
+                driver.execute_script(f"window.scrollTo(0, {height * 1.7});")  # 90
+                time.sleep(time_)
+                driver.execute_script(f"window.scrollTo(0, {height * 2.1});")  # 100
+                time.sleep(time_)
+
+        goods = WebDriverWait(driver, 20).until(
+            EC.presence_of_all_elements_located((By.CLASS_NAME, 'product-card__link')))
+        links = [good.get_attribute('href') for index, good in enumerate(goods, start=1) if index <= quantity]
+    except Exception as err:
+        print(err)
+        driver.quit()
+        return [None]
+    time.sleep(random.randrange(1, 15))
+    driver.quit()
+    return links
+
+
+def extract_data(search_name: str, sort: str, quantity: int) -> list:
+    args = [(PRODUCTS_PER_LOADED_PAGE, get_valid_url(search_name, sort, i+1)) for i in range(quantity // PRODUCTS_PER_LOADED_PAGE)]
+    if quantity % PRODUCTS_PER_LOADED_PAGE != 0:
+        args.append((quantity % PRODUCTS_PER_LOADED_PAGE, get_valid_url(search_name, sort, len(args)+1)))
+
+    with mp.Pool(processes=mp.cpu_count()) as p:
+        result = p.starmap(get_links, args)
+        
+    new_args = [i for sublist in result for i in sublist]
+    with mp.Pool(processes=mp.cpu_count()) as p:
+        new_result = p.map(get_info, new_args)
+
+    return new_result
+
+
 if __name__ == "__main__":
-    data = extract_data('Iphone 12', 25, 'popular')
-    print(data)
+    data = extract_data('Iphone 12', 'popular', 134)   
